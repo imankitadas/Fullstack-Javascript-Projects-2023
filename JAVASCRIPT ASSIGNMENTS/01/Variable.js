5e4553ceead0b47898e896900b1d6cc0a004e245
imankitadas <ankitaab07@GMAIL.com>
2023-07-12 23:45:23 +0530

    new file updated

---
+++ JAVASCRIPT ASSIGNMENTS/01/Variable.js
@@ -0,0 +1,17 @@
+// 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+// "typeof" => keyword is used to see the type of variable. 
+//  console.log => is used to print the output on the screen.
+
+
+let userName = 'Ankita';   //string
+console.log("Data type of", userName, "is", typeof userName);
+
+let isRegistered = false;  //boolean
+console.log("Data type of", isRegistered , "is", typeof isRegistered);
+
+let courseName;  //undefined
+console.log("Data type of courseName is", typeof courseName);
+
+let testScore = null;  //null
+console.log("Data type of testScore is", typeof testScore);
