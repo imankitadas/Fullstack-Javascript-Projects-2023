4553884a85359298fdef7ef1645b73ece4f3d65d
Ankita Das <ankitaab07@gmail.com>
2023-07-25 19:06:27 +0530

    Update index.js

--- JAVASCRIPT ASSIGNMENTS/01_QnA/10/index.js
+++ JAVASCRIPT ASSIGNMENTS/01_QnA/10/index.js
@@ -1,49 +1,38 @@
-     // 10. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+// 10. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
 
 
  // - 4 > 3
 console.log(`The result of the comparison 4 > 3 is: ${4 > 3}`);   //true  
 
-
 // - 4 >= 3
 console.log(`The result of the comparison 4 >= 3 is: ${4 >= 3}`);   //true  
-
 
 // - 4 < 3
 console.log(`The result of the comparison 4 < 3 is: ${4 < 3}`);   //false
 
-
 // - 4 <= 3
 console.log(`The result of the comparison 4 <= 3 is: ${4 <= 3}`);   //false
-
 
 // - 4 == 4
 console.log(`The result of the comparison 4 == 4 is: ${4 == 4}`);   //true
 
-
 // - 4 === 4
 console.log(`The result of the comparison 4 == 4 is: ${4 == 4}`);   //true
-
 
 // - 4 != 4
 console.log(`The result of the comparison 4 != 4 is: ${4 != 4}`);   //false
   
-
 // - 4 !== 4
 console.log(`The result of the comparison 4 !== 4 is: ${4 !== 4}`);   //false   
-
 
 // - 4 != '4'
 console.log(`The result of the comparison 4 != '4' is: ${4 != '4'}`);   //false
 
-
 // - 4 == '4'
 console.log(`The result of the comparison 4 == '4' is: ${4 == '4'}`);   //true
 
-
 // - 4 === '4'
 console.log(`The result of the comparison 4 === '4' is: ${4 === '4'}`);   //false
-
 
 
 // - Find the length of python and jargon and make a falsy comparison statement.
