662b54a5f2aef396cc3958c10b0d1cdd8db9868c
imankitadas <ankitaab07@GMAIL.com>
2023-12-01 23:49:14 +0530

    added

---
+++ 03_JAVASCRIPT ASSIGNMENTS/04_DOM Assignments/01_DOM Assignment 2.0 1,2,3/contact/contact.js
@@ -0,0 +1,16 @@
+// DOM Third Assignment 
+
+// <-------------- Task 1 --------------->
+
+
+// Changing the User Output Placeholder
+
+document.querySelector(".enterName").placeholder = "FSJS2.0";
+document.querySelector(".enterMail").placeholder = "fsjs@ineuron.ai";
+document.querySelector(".enterMessage").placeholder = "Hello World";
+
+// Changing the User Input Placeholder
+
+document.querySelector(".userName").placeholder = "FSJS2.0";
+document.querySelector(".userEmail").placeholder = "fsjs@ineuron.ai";
+document.querySelector(".userMessage").placeholder = "Hello World";
